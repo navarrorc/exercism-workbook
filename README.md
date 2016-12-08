d2aef80063b42a99e8b3d37482e581f9aed9536f
Roberto <roberto@austincodingacademy.com>
2016-12-08 03:59:01 +0000

    Initial Commit

---
+++ README.md
@@ -0,0 +1,8 @@
+This workbook will setup exercism.io so you can start working on challenges quickly.
+
+Before continuing make sure to open **setup.sh** and add YOUR_API_KEY from exercism.io, you can find you key [here](http://exercism.io/account/key) save the file and continue reading this guide.
+
+In the terminal type `./setup.sh`
+If you get an error, then type `chmod 755 setup.sh` and then try running `./setup.sh` again.
+
+Congratulations! You are ready to go, read this [guide](http://exercism.io/languages/javascript/exercises) to download your first challenge.
---
+++ setup.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Step 1
+cd ./_additional-resources
+tar -xzvf exercism-linux-64bit.tgz
+
+# Step 2
+mkdir ~/bin
+mv exercism ~/bin/
+export PATH=$HOME/bin:$PATH
+
+# Step 3
+echo 'export PATH=$HOME/bin:$PATH' >> ~/.bashrc
+
+# Step 4
+# Uncomment the line below and make sure you provide your own API Key, see http://exercism.io/cli/linux
+#exercism configure --key=YOUR_API_KEY
+
+# Step 5
+# You can change this location but not recommended, DO NOT CHANGE! unless you know what you are doing
+exercism configure --dir=~/workspace/exercism-workbook
+
+#exercism --version
+
+npm install jasmine-node -g
+
+
