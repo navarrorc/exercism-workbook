5fb202fe55112f938b399fb4ed193b65e4cc2660
Roberto <rnavarro70@gmail.com>
2017-07-05 15:47:30 +0000

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Step 1
-cd ./_additional-resources
+cd ./.resources
 tar -xzvf exercism-linux-64bit.tgz
 
 # Step 2
@@ -10,11 +10,13 @@ mv exercism ~/bin/
 export PATH=$HOME/bin:$PATH
 
 # Step 3
-echo 'export PATH=$HOME/bin:$PATH' >> ~/.bashrc
+echo "" >> ~/.bashrc
+echo "export PATH=$HOME/bin:$PATH" >> ~/.bashrc
+
 
 # Step 4
 # Uncomment the line below and make sure you provide your own API Key, see http://exercism.io/cli/linux
-#exercism configure --key=YOUR_API_KEY
+exercism configure --key=8164ec9f238e44baab0ad3e54fa4162c
 
 # Step 5
 # You can change this location but not recommended, DO NOT CHANGE! unless you know what you are doing
